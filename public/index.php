45dab4e27accf864b23d68ac10509d279e6d90f5
Florin Iacob <florin@n5040.sinapticode.local>
2012-10-05 17:13:01 +0300

    &37250045  , #11373 -- add GA to prelaunch

--- public/index.php
+++ public/index.php
@@ -691,5 +691,22 @@ mixpanel.init("<?php $key_file = dirname(__FILE__) . '/../mp_domain.key';
     
 </div><!-- wrap -->
 
+
+<?php if (file_exists($ga_file = (dirname(__FILE__).'/../ga_domain.key') )
+        and ($ga_key = trim(file_get_contents($ga_file)))
+    ) { ?>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?php echo $ga_key; ?>']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<?php } ?>
+
 </body>
 </html>
